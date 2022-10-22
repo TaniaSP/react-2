d8991a82071d1b24bf4700ac01b0fa25cf5a643a
Tania Soto Pineda <taniasotopineda92@gmail.com>
2022-10-22 15:43:40 -0600

    husky

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import { User } from '../models/interfaces'
 import './app.scss'
 
-export default function App ({ user }: { user: User }) {
+export default function App ({ user }: { user: User }): JSX.Element {
   return <h1>Welcome, <span>{user.name}</span>!! <sup>{user.age}</sup></h1>
 }
