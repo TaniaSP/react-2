582351197a4abb0fecb0180a2422b7c531b6d5c1
Tania Soto Pineda <taniasotopineda92@gmail.com>
2022-10-22 16:06:12 -0600

    lint-staged

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,5 +3,5 @@ import { User } from '../models/interfaces'
 import './app.scss'
 
 export default function App ({ user }: { user: User }): JSX.Element {
-  return <h1>Welcome, <span>{user.name}</span>!! <sup>{user.age}</sup></h1>
+  return <h1>Welcome, <span>{user.name}</span><sup>{user.age}</sup></h1>
 }
